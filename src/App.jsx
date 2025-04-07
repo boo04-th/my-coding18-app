3f08e4e710e1c252bafe182b1efee44a658b815d
Hient <hient@usf.edu>
2025-04-06 21:56:54 -0400

    Task 2: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Greetings from "./components/Greetings";
+import React from "react"; //This is the main component of the React app. It shows logos, a counter button, and the Greeting component
+import Greetings from "./components/Greetings"; //Greeting component is imported from the components folder
 
-function App() {
+function App() { //This is the main function that defines the App component
   return (
     <div>
       <h1>Welcome to Our Shop</h1>
-      <Greetings name="Best Matcha ever" />
-      <h2>Products</h2>
-        <Products name="Matcha latte with Isuzu powder" price="$5" />
-        <Products name="Matcha latte with Unkaku powder" price="$7" />
-        <Products name="Matcha latte with Wako powder" price="$9" />
+      <p>
+      <Greetings name="Hien Tran" />
+      <Greetings name="Matcha latte with Isuzu powder" price="$5" />
+      <Greetings name="Matcha latte with Unkaku powder" price="$7" />
+      <Greetings name="Matcha latte with Wako powder" price="$9" /></p>
     </div>
   );
 }
 
-export default App;
+export default App; //Exporting the App component so it can be used in other files
--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,12 +1,12 @@
-import React from 'react'; // Importing React to Create React Component
+import React from "react"; // Importing React to Create React Component
 
 // Creating a new component called Greetings
-function Greeting ({name}){
+function Greetings ({name}){
     return(
         <div>
-            <h2>Hello from the Greeting Component!</h2> 
-            <p>This is your first custom component in React.</p>
-        </div>
+         <h3>Hello,{name}!</h3>
+         <p>Please take a look at our drinks</p>       
+         </div>
     );
 }
-export default Greeting; // Exporting the Greetings component so it can be used in app.jsx
+export default Greetings; // Exporting the Greetings component so it can be used in app.jsx
