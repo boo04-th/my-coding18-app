f67f9f6462793a5e70d126d8ce5f6dbe2dee1fbe
Hient <hient@usf.edu>
2025-04-06 21:21:46 -0400

    Renam to greetings

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Greetings from "./components/Greetings";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Welcome to Our Shop</h1>
+      <Greetings name="Best Matcha ever" />
+      <h2>Products</h2>
+        <Products name="Matcha latte with Isuzu powder" price="$5" />
+        <Products name="Matcha latte with Unkaku powder" price="$7" />
+        <Products name="Matcha latte with Wako powder" price="$9" />
+    </div>
+  );
 }
 
-export default App
+export default App;
